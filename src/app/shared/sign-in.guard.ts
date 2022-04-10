74f85fd340cf48be10360df332dfbe2753a138eb
Keith Hall <hkeith62@gmail.com>
2022-04-10 16:54:15 -0500

    Sign-in guard

---
+++ src/app/shared/sign-in.guard.ts
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title:  sign-in.guard.ts
+; Author: Professor Krasso
+; Date: 10 April 2022
+; Modified By: K Hall
+; Description: Sign-in guard for Nodebucket.
+;===========================================
+*/
+
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignInGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+
+}
